53f6c64a45a3e71d018d2ae1be98cb8bdad4315f
Maria <emailtomariageorge@gmail.com>
2024-06-27 09:00:59 -0700

    4-5-2

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import random
-
-
 def main():
+    id = 0
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    while ( total < 100):
+        num = random.randint(0,100)
+        numbers.append(num)
+        total += numbers[id]
+        id = id + 1
+    print('Output ')
+    print(numbers)
+    print(f'The total sum is {total-num}')
+    
+
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
